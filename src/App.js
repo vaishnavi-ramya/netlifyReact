8bae18d831a0f7dafba9a787c29ad2036bc068cf
Your Name <you@example.com>
2020-05-04 08:04:27 +0530

    connected netlify

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Welcome to build</h1>
+        <h1>deployes successfully</h1>
       </div>
     </div>
   );
